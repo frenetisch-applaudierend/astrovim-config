89ff744332ebb68a69ceb473ee849e088e0db3f3
Markus Gasser <markus.gasser@mailbox.org>
2024-02-25 10:49:30 +0100

    Add rust plugin and use catppuccin theme

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,15 @@
 return {
   -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  "AstroNvim/astrocommunity",
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.rust" },
+
+  {
+    "catppuccin",
+    opts = {
+      flavour = "macchiato"
+    }
+  }
 }
